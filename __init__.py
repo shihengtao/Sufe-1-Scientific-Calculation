cf3b67f0848cca76011dc97a00b013c3ebbd8d84
shihengtao <42565294+shihengtao@users.noreply.github.com>
2020-04-17 22:02:43 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import BasicCalculator as BC
+import ScientificCalculator as SC
+
+window = Tk()
+window.title('Sufe-1-Scientific-Calculator')
+window.geometry('500x500')
+
+def Basic():
+    BC.Basic()
+
+
+def Science():
+    SC.Science()
+
+
+def Business():
+    pass
+
+
+def SpecificMath():
+    pass
+
+
+Button(window, text="Basic-Calculator", command=Basic, height=1, width=6).\
+    grid(row=0, column=0, padx=30, pady=30, ipadx=60, ipady=60)
+
+Button(window, text="Scientific-Calculator", command=Science, height=1, width=6).\
+    grid(row=0, column=1, padx=30, pady=30, ipadx=60, ipady=60)
+
+Button(window, text="Business-Analysis-Calculator", command=None, height=1, width=6).\
+    grid(row=1, column=0, padx=30, pady=30, ipadx=60, ipady=60)
+
+Button(window, text="Specific-Maths-Calculator", command=None, height=1, width=6).\
+    grid(row=1, column=1, padx=30, pady=30, ipadx=60, ipady=60)
+
+
+window.mainloop()
